595f1f07b1500fb09fa56dd8a3f58ef94278336e
Eitan Bakirov <36690071+EitanBakirov@users.noreply.github.com>
2025-04-29 00:01:53 +0300

    Update data.js

--- user-data/data.js
+++ user-data/data.js
@@ -119,17 +119,13 @@ export const footer = [
   {
     label: "Links",
     data: [
-        {
-          text: "Linkedin",
-          link: "https://www.linkedin.com/in/eitanbakirov/",
-        },
       {
-        text: "Buy me a coffee",
-        link: "https://www.buymeacoffee.com/r194dME8y",
+        text: "Linkedin",
+        link: "https://www.linkedin.com/in/eitanbakirov/",
       },
       {
         text: "Github",
-        link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
+        link: "https://github.com/EitanBakirov/EitanBakirov.github.io",
       },
     ],
   },
